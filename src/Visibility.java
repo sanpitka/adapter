76932c0c090c3343f6f379a2ca4c214059478042
IskaVirtanen <143444148+IskaVirtanen@users.noreply.github.com>
2023-09-19 10:51:04 +0300

    Update Visibility.java

--- src/Visibility.java
+++ src/Visibility.java
@@ -1,4 +1,18 @@
-
-public class Visibility {
-
+public abstract class Visibility {
+	private boolean inBackground = false;
+	
+	public Visibility() {
+		
+	}
+	
+	public void changeVisibility() {
+		if(inBackground ==false) {
+			this.inBackground = true;
+		}else{
+			this.inBackground = false;
+		}
+	}
+	public boolean isVisible() {
+		return inBackground;
+	}
 }
