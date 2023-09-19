e687d1900b2536f07834958dbdef8ff4d499e765
IskaVirtanen <143444148+IskaVirtanen@users.noreply.github.com>
2023-09-19 10:53:23 +0300

    Create Circle.java

---
+++ src/Circle.java
@@ -0,0 +1,22 @@
+public class Circle extends Visibility implements ShapeBasic {
+
+	public Circle() {		
+	}
+	
+	@Override
+	public void draw() {
+		System.out.println("This is a circle");
+	}
+
+	@Override
+	public void grow() {
+		System.out.println("Growing the size of the circle");
+		
+	}
+
+	@Override
+	public String printText() {
+		return "Circle";
+	}
+	
+}
