a9392dd8437957158555e2dde362475bdbc1ee79
IskaVirtanen <iska.virtanen@student.oulu.fi>
2023-10-10 10:53:30 +0300

    Comments added to ShapeBasic.java

--- src/ShapeBasic.java
+++ src/ShapeBasic.java
@@ -1,7 +1,21 @@
 public interface ShapeBasic {
 
+    /**Draws the Shape by Printing the Shape on screen
+     * 
+     */
     public void draw();
+    /**Prints Growing the Shape
+     * 
+     */
     public void grow();
+    /**
+     * 
+     * @return The Shape name as A string
+     */
     public String printText();
+    /**
+     * 
+     * @return if shape is in Visible return true, else return false
+     */
     public boolean isVisible();
 }
