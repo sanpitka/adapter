a1d3c85ed4dc93e3718a734625e4dc70b7706011
sanpitka <sanpitka@student.oulu.fi>
2023-10-10 10:56:54 +0300

    Comments added to ShapeSpecial class

--- src/ShapeSpecial.java
+++ src/ShapeSpecial.java
@@ -1,6 +1,19 @@
 public interface ShapeSpecial {
 
+    /**
+     * Calculates the area of the shape
+     * @return area
+     */
     public double calculateArea();
+
+    /**
+     * Calculates the perimeter of the shape
+     * @return perimeter
+     */
     public double calculatePerimeter();
+
+    /**
+     * "Draws" the shape by printing the name, area, perimeter, and color of the shape
+     */
     public void drawShape();
 }
